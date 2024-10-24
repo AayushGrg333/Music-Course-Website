e02787b935e25f97652c4d558d99c91263a11442
AayushGrg333 <aayushgrg333@gmail.com>
2024-10-24 10:10:37 +0545

    initial Commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+'use Client'
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/UI/navbar-menu";
+import { cn } from "@/Utils/cn";
+ 
+
+
+const Navbar: React.FC = ()=>{
+    return (
+        <div>
+            <h1>This is a Navbar</h1>
+        </div>
+    )
+}
+
+
+export default Navbar
